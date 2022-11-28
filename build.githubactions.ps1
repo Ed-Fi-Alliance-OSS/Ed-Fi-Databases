31e6847b17ec8eb763f36063652c60e4abfee368
m.semalaiappan <m.semalaiappan@gmail.com>
2022-11-28 12:28:59 -0600

    restore method added

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -7,7 +7,7 @@
 param(
     # Command to execute, defaults to "Build".
     [string]
-    [ValidateSet("DotnetClean", "Build", "Test", "Pack", "Publish", "CheckoutBranch")]
+    [ValidateSet("Restore", "DotnetClean", "Build", "Test", "Pack", "Publish", "CheckoutBranch")]
     $Command = "Build",
 
     [switch] $SelfContained,
@@ -115,6 +115,12 @@ function Invoke-Main {
     }
 }
 
+function Restore {
+    Invoke-Execute {
+        dotnet restore $Solution
+    }
+}
+
 function DotnetClean {
     Invoke-Execute { dotnet clean $Solution -c $Configuration --nologo -v minimal }
 }
@@ -217,6 +223,10 @@ function Invoke-DotnetClean {
     Invoke-Step { DotnetClean }
 }
 
+function Invoke-Restore {
+    Invoke-Step { Restore }
+}
+
 function Invoke-Publish {
     Invoke-Step { Publish }
 }
@@ -235,6 +245,7 @@ function Invoke-CheckoutBranch {
 
 Invoke-Main {
     switch ($Command) {
+        Restore { Invoke-Restore }
         DotnetClean { Invoke-DotnetClean }
         Build { Invoke-Build }
         Test { Invoke-Tests }
